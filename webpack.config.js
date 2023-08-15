6e10ab24cebf8817c3e825a15494e9824ad5f527
Stepan Mareev <yaraspik@ya.ru>
2023-08-15 23:50:05 +0300

    edited js-2

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,9 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  devtool: 'source-map',
   watch: true,
   entry: './src/index.js',
-  mode: 'development',
+  mode: 'production',
   output: {
     path: path.resolve(__dirname, 'dist'),
   },
